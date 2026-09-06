0f8d8b490e61799abd7d0d884548c90366d9b1cd
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 16:31:44 +0400

    feat(services): AppointmentService added addAppointment

--- src/main/java/com/CV/Hospital/services/AppointmentService.java
+++ src/main/java/com/CV/Hospital/services/AppointmentService.java
@@ -1,10 +1,15 @@
 package com.CV.Hospital.services;
 
+import com.CV.Hospital.entities.Appointment;
+import com.CV.Hospital.entities.Doctor;
+import com.CV.Hospital.entities.Patient;
 import com.CV.Hospital.repositories.AppointmentRepository;
 import com.CV.Hospital.repositories.DoctorRepository;
 import com.CV.Hospital.repositories.PatientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -14,4 +19,26 @@ public class AppointmentService {
     private final DoctorRepository doctorRepository;
     private final PatientRepository patientRepository;
 
+
+    public Appointment addAppointment(
+            Appointment appointment,
+            Long doctorId,
+            Long patientId) {
+        Doctor doctor = doctorRepository.findByIdAndIsActiveTrue(doctorId)
+                .orElseThrow(() -> new RuntimeException("Doctor not found"));
+        Patient patient = patientRepository.findByIdAndIsActiveTrue(patientId)
+                .orElseThrow(() -> new RuntimeException("Patient not found"));
+        if (appointment.getAppointmentDate() == null) {
+            throw new IllegalArgumentException("Appointment date is required");
+        }
+        if (appointment.getAppointmentDate().isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException(
+                    "Appointment date cannot be in the past"
+            );
+        }
+        appointment.setDoctor(doctor);
+        appointment.setPatient(patient);
+        appointment.setIsActive(true);
+        return appointmentRepository.save(appointment);
+    }
 }
