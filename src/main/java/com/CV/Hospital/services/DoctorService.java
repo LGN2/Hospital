ca78d0b422b4fe990d1e5530313ed5a9debb1b9b
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 16:20:35 +0400

    feat(services): DoctorService added add doctor

--- src/main/java/com/CV/Hospital/services/DoctorService.java
+++ src/main/java/com/CV/Hospital/services/DoctorService.java
@@ -1,5 +1,8 @@
 package com.CV.Hospital.services;
 
+import com.CV.Hospital.entities.Department;
+import com.CV.Hospital.entities.Doctor;
+import com.CV.Hospital.entities.Hospital;
 import com.CV.Hospital.repositories.DepartmentRepository;
 import com.CV.Hospital.repositories.DoctorRepository;
 import com.CV.Hospital.repositories.HospitalRepository;
@@ -13,4 +16,27 @@ public class DoctorService {
     private final DoctorRepository doctorRepository;
     private final DepartmentRepository departmentRepository;
     private final HospitalRepository hospitalRepository;
+
+    public Doctor addDoctor(
+            Doctor doctor,
+            Long departmentId,
+            Long hospitalId) {
+        Department department =
+                departmentRepository.findByIdAndIsActiveTrue(departmentId)
+                        .orElseThrow(() ->
+                                new RuntimeException("Department not found"));
+        Hospital hospital =
+                hospitalRepository.findByIdAndIsActiveTrue(hospitalId)
+                        .orElseThrow(() ->
+                                new RuntimeException("Hospital not found"));
+        if (!department.getHospital().getId().equals(hospital.getId())) {
+            throw new IllegalArgumentException(
+                    "Department does not belong to the selected hospital"
+            );
+        }
+        doctor.setDepartment(department);
+        doctor.setHospital(hospital);
+        doctor.setIsActive(true);
+        return doctorRepository.save(doctor);
+    }
 }
