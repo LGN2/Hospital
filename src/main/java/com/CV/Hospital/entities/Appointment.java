30f4011ec66b4544e97ce4ae75db94ec8917f075
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 11:43:48 +0400

    feat(entities): Appointment added @Column with nullable = false for appointmentDate with LocalDateTime type

--- src/main/java/com/CV/Hospital/entities/Appointment.java
+++ src/main/java/com/CV/Hospital/entities/Appointment.java
@@ -1,10 +1,13 @@
 package com.CV.Hospital.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "appointments")
@@ -12,4 +15,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Appointment extends BaseClass{
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentDate;
 }
