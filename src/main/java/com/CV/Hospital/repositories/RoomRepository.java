a651b4a7285f16ad619eb588d26b119d7c50f348
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 15:12:49 +0400

    feat(repositories): finished RoomRepository

--- src/main/java/com/CV/Hospital/repositories/RoomRepository.java
+++ src/main/java/com/CV/Hospital/repositories/RoomRepository.java
@@ -1,4 +1,16 @@
 package com.CV.Hospital.repositories;
 
-public interface RoomRepository {
+import com.CV.Hospital.entities.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RoomRepository
+        extends JpaRepository<Room, Long> {
+    List<Room> findByIsActiveTrue();
+    Optional<Room> findByIdAndIsActiveTrue(Long id);
+    List<Room> findByHospitalIdAndIsActiveTrue(Long hospitalId);
 }
