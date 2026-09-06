e10b41696be827a0b65a91b84e7318795a1e6487
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 15:03:25 +0400

    feat(repositories): finished  DepartmentRepository

--- src/main/java/com/CV/Hospital/repositories/DepartmentRepository.java
+++ src/main/java/com/CV/Hospital/repositories/DepartmentRepository.java
@@ -1,4 +1,16 @@
 package com.CV.Hospital.repositories;
 
-public interface DepartmentRepository {
+import com.CV.Hospital.entities.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface DepartmentRepository
+        extends JpaRepository<Department, Long> {
+    List<Department> findByIsActiveTrue();
+    Optional<Department> findByIdAndIsActiveTrue(Long id);
+    List<Department> findByHospitalIdAndIsActiveTrue(Long hospitalId);
 }
