cf081d54b73452e076ef5b88ec6d768a240e382d
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 14:29:36 +0400

    feat(entities): Bill added amount

--- src/main/java/com/CV/Hospital/entities/Bill.java
+++ src/main/java/com/CV/Hospital/entities/Bill.java
@@ -1,10 +1,13 @@
 package com.CV.Hospital.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "bills")
@@ -12,4 +15,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Bill extends BaseClass{
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal amount;
 }
