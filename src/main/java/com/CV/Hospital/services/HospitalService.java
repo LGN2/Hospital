3d74799776ed4baee7ed8336b9721be334420e28
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 15:23:54 +0400

    feat(services):starting on HospitalService

--- src/main/java/com/CV/Hospital/services/HospitalService.java
+++ src/main/java/com/CV/Hospital/services/HospitalService.java
@@ -1,4 +1,12 @@
 package com.CV.Hospital.services;
 
+import com.CV.Hospital.repositories.HospitalRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class HospitalService {
+
+    private final HospitalRepository hospitalRepository;
 }
