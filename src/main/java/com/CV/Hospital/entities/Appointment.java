d6c5b6cf12d6e4ee7d20db9af33fabfc18190e90
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 11:45:54 +0400

    feat(entities): Appointment added @Column with nullable = false and length while also adding @Enumerated with enumtype string for AppointmentStatusType

--- src/main/java/com/CV/Hospital/entities/Appointment.java
+++ src/main/java/com/CV/Hospital/entities/Appointment.java
@@ -1,8 +1,7 @@
 package com.CV.Hospital.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.CV.Hospital.entities.type.AppointmentStatusType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,4 +20,8 @@ public class Appointment extends BaseClass{
 
     @Column(nullable = false, length = 500)
     private String reason;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private AppointmentStatusType status;
 }
