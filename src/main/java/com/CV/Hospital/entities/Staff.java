0814f0bc143a676c05d340a11ec665df7b280d4f
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 14:37:33 +0400

    feat(entities): Staff added entity, table name, getters, setters and extended to baseclass

--- src/main/java/com/CV/Hospital/entities/Staff.java
+++ src/main/java/com/CV/Hospital/entities/Staff.java
@@ -1,4 +1,15 @@
 package com.CV.Hospital.entities;
 
-public class Staff {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "staff")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Staff extends BaseClass{
 }
