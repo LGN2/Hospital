0ab1ff567197d29798b45bb7e54af1dd14741811
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 11:17:03 +0400

    feat(entities): Patient added @entity, table name, getter, setter and noargsconstructor while extending baseclass

--- src/main/java/com/CV/Hospital/entities/Patient.java
+++ src/main/java/com/CV/Hospital/entities/Patient.java
@@ -1,4 +1,15 @@
 package com.CV.Hospital.entities;
 
-public class Patient {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "patients")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Patient extends BaseClass{
 }
