39f2a9af0af8f2e25c719e9ec9b25f254f5834ad
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 15:11:45 +0400

    feat(repositories): finished PrescriptionRepository

--- src/main/java/com/CV/Hospital/repositories/PrescriptionRepository.java
+++ src/main/java/com/CV/Hospital/repositories/PrescriptionRepository.java
@@ -1,4 +1,16 @@
 package com.CV.Hospital.repositories;
 
-public interface PrescriptionRepository {
+import com.CV.Hospital.entities.Prescription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PrescriptionRepository
+        extends JpaRepository<Prescription, Long> {
+    List<Prescription> findByIsActiveTrue();
+    Optional<Prescription> findByIdAndIsActiveTrue(Long id);
+    List<Prescription> findByMedicalRecordIdAndIsActiveTrue(Long medicalRecordId);
 }
