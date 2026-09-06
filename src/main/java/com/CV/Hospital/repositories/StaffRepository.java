f9ec74db5f5fa531a2f7797799d25568fa5017a3
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 15:18:19 +0400

    feat(repositories):finished StaffRepository

--- src/main/java/com/CV/Hospital/repositories/StaffRepository.java
+++ src/main/java/com/CV/Hospital/repositories/StaffRepository.java
@@ -1,4 +1,16 @@
 package com.CV.Hospital.repositories;
 
-public interface StaffRepository {
+import com.CV.Hospital.entities.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface StaffRepository
+        extends JpaRepository<Staff, Long> {
+    List<Staff> findByIsActiveTrue();
+    Optional<Staff> findByIdAndIsActiveTrue(Long id);
+    List<Staff> findByDepartmentIdAndIsActiveTrue(Long departmentId);
 }
