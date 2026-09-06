f609a3431ec966e5421566ff9e04a0313af319e2
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 15:13:45 +0400

    feat(repositories): finished AdmissionRepository

--- src/main/java/com/CV/Hospital/repositories/AdmissionRepository.java
+++ src/main/java/com/CV/Hospital/repositories/AdmissionRepository.java
@@ -1,4 +1,18 @@
 package com.CV.Hospital.repositories;
 
-public interface AdmissionRepository {
+import com.CV.Hospital.entities.Admission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AdmissionRepository
+        extends JpaRepository<Admission, Long> {
+    List<Admission> findByIsActiveTrue();
+    Optional<Admission> findByIdAndIsActiveTrue(Long id);
+    List<Admission> findByPatientIdAndIsActiveTrue(Long patientId);
+    List<Admission> findByRoomIdAndIsActiveTrue(Long roomId);
+    List<Admission> findByRoomIdAndDischargeDateIsNullAndIsActiveTrue(Long roomId);
 }
