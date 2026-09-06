92436a5e37589f023cbffb4471a635b0075ae206
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 11:19:13 +0400

    feat(entities): Patient added @Column with nullable false and length for name

--- src/main/java/com/CV/Hospital/entities/Patient.java
+++ src/main/java/com/CV/Hospital/entities/Patient.java
@@ -1,5 +1,6 @@
 package com.CV.Hospital.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -12,4 +13,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Patient extends BaseClass{
+
+    @Column(nullable = false, length = 100)
+    private String name;
 }
