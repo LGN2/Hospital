a7466275e1e728d8bc5dc83410644c5faee9ef0c
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 14:24:18 +0400

    feat(entities): Admission added @column for admitDate

--- src/main/java/com/CV/Hospital/entities/Admission.java
+++ src/main/java/com/CV/Hospital/entities/Admission.java
@@ -1,10 +1,13 @@
 package com.CV.Hospital.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "admissions")
@@ -12,4 +15,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Admission extends BaseClass{
+
+    @Column(nullable = false)
+    private LocalDate admitDate;
 }
