37cceebaa3a9cf97f0c155ba307bac8a1178407e
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 10:59:27 +0400

    feat(entities): Department added the attributes with there neccessary @

--- src/main/java/com/CV/Hospital/entities/Department.java
+++ src/main/java/com/CV/Hospital/entities/Department.java
@@ -1,10 +1,11 @@
 package com.CV.Hospital.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "departments")
@@ -13,4 +14,19 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Department extends BaseClass{
 
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "hospital_id", nullable = false)
+    private Hospital hospital;
+
+    @OneToMany(mappedBy = "department")
+    private List<Doctor> doctors = new ArrayList<>();
+
+    @OneToMany
+    private List<Staff> staff = new ArrayList<>();
 }
