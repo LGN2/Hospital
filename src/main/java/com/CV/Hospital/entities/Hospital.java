40e0f20f88cdf394d95162b641bf3b3103fc1f2a
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 10:45:42 +0400

    feat(entities): Hospital added all the attributes

--- src/main/java/com/CV/Hospital/entities/Hospital.java
+++ src/main/java/com/CV/Hospital/entities/Hospital.java
@@ -1,11 +1,37 @@
 package com.CV.Hospital.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @Entity
+@Table(name = "hospitals")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Hospital extends BaseClass{
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 150)
+    private String location;
+
+    @OneToMany(mappedBy = "hospital")
+    private List<Department> departments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "hospital")
+    private List<Doctor> doctors = new ArrayList<>();
+
+    @OneToMany(mappedBy = "hospital")
+    private List<Patient> patients = new ArrayList<>();
+
+    @OneToMany(mappedBy = "hospital")
+    private List<Room> rooms = new ArrayList<>();
 }
