b2e13d9a310c74a9b8a7c0e5bf750a2b27bf8ff7
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 09:30:29 +0400

    feat(entities): BaseClass added @Id and @GeneratedValue

--- src/main/java/com/CV/Hospital/entities/BaseClass.java
+++ src/main/java/com/CV/Hospital/entities/BaseClass.java
@@ -1,5 +1,8 @@
 package com.CV.Hospital.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,5 +10,7 @@ import lombok.Setter;
 @Setter
 public class BaseClass {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 }
