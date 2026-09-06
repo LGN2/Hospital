bfd15d3e68e41fa98f80f2c7f16fa9396fcb553a
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 12:09:37 +0400

    feat(entities): Prescription added @Entity, @Table, @Getter, @Setter and @NoArgsConstructor while extending BaseClass

--- src/main/java/com/CV/Hospital/entities/Prescription.java
+++ src/main/java/com/CV/Hospital/entities/Prescription.java
@@ -1,4 +1,15 @@
 package com.CV.Hospital.entities;
 
-public class Prescription {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "prescriptions")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Prescription extends BaseClass{
 }
