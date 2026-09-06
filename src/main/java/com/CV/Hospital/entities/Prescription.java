46dc74060e2d55055fe964f43c4327b7193d2cfe
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 12:14:39 +0400

    feat(entities): Perscription added @ManyToOne for MedicalRecord also @JoinColumn for medical id

--- src/main/java/com/CV/Hospital/entities/Prescription.java
+++ src/main/java/com/CV/Hospital/entities/Prescription.java
@@ -1,8 +1,6 @@
 package com.CV.Hospital.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,4 +20,8 @@ public class Prescription extends BaseClass{
 
     @Column(nullable = false)
     private Integer durationDays;
+
+    @ManyToOne
+    @JoinColumn(name = "medical_record_id", nullable = false)
+    private MedicalRecord medicalRecord;
 }
