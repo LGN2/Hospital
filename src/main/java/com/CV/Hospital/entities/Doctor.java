0df32648e9fba5732afd34061cb0d3e895067cc6
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 11:03:54 +0400

    feat(entities): Doctor added the @ for entitry, table , getter, setter, noargscontsructor while also extending baseclass

--- src/main/java/com/CV/Hospital/entities/Doctor.java
+++ src/main/java/com/CV/Hospital/entities/Doctor.java
@@ -1,4 +1,15 @@
 package com.CV.Hospital.entities;
 
-public class Doctor {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "doctors")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Doctor extends BaseClass{
 }
