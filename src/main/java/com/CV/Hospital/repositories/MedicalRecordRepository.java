9ce3ac71e88e83e99dfb7cbc0f991f47a6c71e95
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 15:10:34 +0400

    feat(repositories): finished MedicalRecordRepository

--- src/main/java/com/CV/Hospital/repositories/MedicalRecordRepository.java
+++ src/main/java/com/CV/Hospital/repositories/MedicalRecordRepository.java
@@ -1,4 +1,16 @@
 package com.CV.Hospital.repositories;
 
-public interface MedicalRecordRepository {
+import com.CV.Hospital.entities.MedicalRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MedicalRecordRepository
+        extends JpaRepository<MedicalRecord, Long> {
+    List<MedicalRecord> findByIsActiveTrue();
+    Optional<MedicalRecord> findByIdAndIsActiveTrue(Long id);
+    List<MedicalRecord> findByPatientIdAndIsActiveTrue(Long patientId);
 }
