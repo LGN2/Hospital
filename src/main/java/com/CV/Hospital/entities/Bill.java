1a5b7493ebbf3679a0a5c28375aace0352892471
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 14:29:08 +0400

    feat(entities): Bill added getter,setter and extended to baseclass

--- src/main/java/com/CV/Hospital/entities/Bill.java
+++ src/main/java/com/CV/Hospital/entities/Bill.java
@@ -1,4 +1,15 @@
 package com.CV.Hospital.entities;
 
-public class Bill {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "bills")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Bill extends BaseClass{
 }
