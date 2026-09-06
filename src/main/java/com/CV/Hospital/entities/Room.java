75eb921d6b10bdd651a593919f7d011abe9efd57
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 12:18:33 +0400

    feat(entities): Room added @Entity, @Table, @Getter, @Setter and @NoArgsConstructor while extending Baseclass

--- src/main/java/com/CV/Hospital/entities/Room.java
+++ src/main/java/com/CV/Hospital/entities/Room.java
@@ -1,4 +1,15 @@
 package com.CV.Hospital.entities;
 
-public class Room {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "rooms")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Room extends BaseClass{
 }
