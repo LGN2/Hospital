ca4b3e76104005ab02fc88cb37567280835747d9
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 15:16:47 +0400

    feat(repositories): finished GuardianRepository

--- src/main/java/com/CV/Hospital/repositories/GuardianRepository.java
+++ src/main/java/com/CV/Hospital/repositories/GuardianRepository.java
@@ -1,4 +1,16 @@
 package com.CV.Hospital.repositories;
 
-public interface GuardianRepository {
+import com.CV.Hospital.entities.Guardian;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GuardianRepository
+        extends JpaRepository<Guardian, Long> {
+    List<Guardian> findByIsActiveTrue();
+    Optional<Guardian> findByIdAndIsActiveTrue(Long id);
+    List<Guardian> findByPatientIdAndIsActiveTrue(Long patientId);
 }
