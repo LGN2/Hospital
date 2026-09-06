0ebb00e42d301d9a183a72f658d7e78c8ea14a3b
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 11:51:15 +0400

    feat(entities): MedicalRecord  added @Entity, @Table, @Getter, @Setter and @NoArgsConstructor while extending BaseClass

--- src/main/java/com/CV/Hospital/entities/MedicalRecord.java
+++ src/main/java/com/CV/Hospital/entities/MedicalRecord.java
@@ -1,4 +1,15 @@
 package com.CV.Hospital.entities;
 
-public class MedicalRecord {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "medical_records")
+@Getter
+@Setter
+@NoArgsConstructor
+public class MedicalRecord extends BaseClass{
 }
