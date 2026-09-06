5204e345e4182b3a391eef51d02fa563c710c434
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 16:14:04 +0400

    feat(services): DepartmentService added obj

--- src/main/java/com/CV/Hospital/services/DepartmentService.java
+++ src/main/java/com/CV/Hospital/services/DepartmentService.java
@@ -1,4 +1,15 @@
 package com.CV.Hospital.services;
 
+import com.CV.Hospital.repositories.DepartmentRepository;
+import com.CV.Hospital.repositories.HospitalRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+    private final HospitalRepository hospitalRepository;
+
 }
