57b289f1000251830310c94a802e079eaf9a8589
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 15:15:42 +0400

    feat(repositories): finished BillRepository

--- src/main/java/com/CV/Hospital/repositories/BillRepository.java
+++ src/main/java/com/CV/Hospital/repositories/BillRepository.java
@@ -1,4 +1,22 @@
 package com.CV.Hospital.repositories;
 
-public interface BillRepository {
+import com.CV.Hospital.entities.Bill;
+import com.CV.Hospital.entities.type.BillStatusType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BillRepository
+        extends JpaRepository<Bill, Long> {
+    List<Bill> findByIsActiveTrue();
+    Optional<Bill> findByIdAndIsActiveTrue(Long id);
+    List<Bill> findByPatientIdAndIsActiveTrue(Long patientId);
+    List<Bill> findByStatusAndIsActiveTrue(BillStatusType status);
+    List<Bill> findByPatientIdAndStatusAndIsActiveTrue(
+            Long patientId,
+            BillStatusType status
+    );
 }
