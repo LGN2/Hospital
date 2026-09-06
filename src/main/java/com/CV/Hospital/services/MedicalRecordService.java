bc56f51cec98b7ab8a078235a661424c4d8477fb
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 16:36:16 +0400

    feat(services): MedicalRecordService added addMedicalRecord

--- src/main/java/com/CV/Hospital/services/MedicalRecordService.java
+++ src/main/java/com/CV/Hospital/services/MedicalRecordService.java
@@ -1,5 +1,7 @@
 package com.CV.Hospital.services;
 
+import com.CV.Hospital.entities.MedicalRecord;
+import com.CV.Hospital.entities.Patient;
 import com.CV.Hospital.repositories.MedicalRecordRepository;
 import com.CV.Hospital.repositories.PatientRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,4 +13,17 @@ public class MedicalRecordService {
 
     private final MedicalRecordRepository medicalRecordRepository;
     private final PatientRepository patientRepository;
+
+    public MedicalRecord addMedicalRecord(
+            MedicalRecord medicalRecord,
+            Long patientId) {
+        Patient patient =
+                patientRepository.findByIdAndIsActiveTrue(patientId)
+                        .orElseThrow(() ->
+                                new RuntimeException("Patient not found"));
+
+        medicalRecord.setPatient(patient);
+        medicalRecord.setIsActive(true);
+        return medicalRecordRepository.save(medicalRecord);
+    }
 }
