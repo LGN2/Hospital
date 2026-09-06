0e573dd63dbc12932531745f1864a16f54460006
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-06 14:30:34 +0400

    feat(entities): Bill added billDate

--- src/main/java/com/CV/Hospital/entities/Bill.java
+++ src/main/java/com/CV/Hospital/entities/Bill.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "bills")
@@ -18,8 +19,10 @@ public class Bill extends BaseClass{
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal amount;
 
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private BillStatusType status;
+
+    @Column(nullable = false)
+    private LocalDate billDate;
 }
