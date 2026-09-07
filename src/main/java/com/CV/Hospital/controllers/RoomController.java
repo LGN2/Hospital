f5643bbb50e4e28c1b2abf39a446304ced44b9df
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 14:48:37 +0400

    feat(controllers): RoomController added addRoom

--- src/main/java/com/CV/Hospital/controllers/RoomController.java
+++ src/main/java/com/CV/Hospital/controllers/RoomController.java
@@ -1,9 +1,9 @@
 package com.CV.Hospital.controllers;
 
+import com.CV.Hospital.entities.Room;
 import com.CV.Hospital.services.RoomService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rooms")
@@ -11,4 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoomController {
 
     private final RoomService roomService;
+
+    @PostMapping
+    public Room addRoom(
+            @RequestBody Room room,
+            @RequestParam Long hospitalId) {
+
+        return roomService.addRoom(room, hospitalId);
+    }
 }
