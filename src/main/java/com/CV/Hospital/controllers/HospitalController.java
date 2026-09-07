a734c44783f626089117b608fcb03211ef927fb7
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 12:02:55 +0400

    feat(controllers): HospitalController added getAllHospitals

--- src/main/java/com/CV/Hospital/controllers/HospitalController.java
+++ src/main/java/com/CV/Hospital/controllers/HospitalController.java
@@ -3,10 +3,9 @@ package com.CV.Hospital.controllers;
 import com.CV.Hospital.entities.Hospital;
 import com.CV.Hospital.services.HospitalService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("hospitals")
@@ -18,5 +17,10 @@ public class HospitalController {
     @PostMapping
     public Hospital addHospital(@RequestBody Hospital hospital) {
         return hospitalService.addHospital(hospital);
+    }
+
+    @GetMapping
+    public List<Hospital> getAllHospitals() {
+        return hospitalService.getAllHospitals();
     }
 }
