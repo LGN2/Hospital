15c5c5f3655daff31cd8373244cf82997f6e6e4c
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 10:38:27 +0400

    feat(services): AdmissionService added admitPatient

--- src/main/java/com/CV/Hospital/services/AdmissionService.java
+++ src/main/java/com/CV/Hospital/services/AdmissionService.java
@@ -1,10 +1,16 @@
 package com.CV.Hospital.services;
 
+import com.CV.Hospital.entities.Admission;
+import com.CV.Hospital.entities.Patient;
+import com.CV.Hospital.entities.Room;
 import com.CV.Hospital.repositories.AdmissionRepository;
 import com.CV.Hospital.repositories.PatientRepository;
 import com.CV.Hospital.repositories.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +18,33 @@ public class AdmissionService {
     private final AdmissionRepository admissionRepository;
     private final PatientRepository patientRepository;
     private final RoomRepository roomRepository;
+
+    public Admission admitPatient(
+            Admission admission,
+            Long patientId,
+            Long roomId) {
+        Patient patient =
+                patientRepository.findByIdAndIsActiveTrue(patientId)
+                        .orElseThrow(() ->
+                                new RuntimeException("Patient not found"));
+        Room room =
+                roomRepository.findByIdAndIsActiveTrue(roomId)
+                        .orElseThrow(() ->
+                                new RuntimeException("Room not found"));
+        List<Admission> currentAdmissions =
+                admissionRepository
+                        .findByRoomIdAndDischargeDateIsNullAndIsActiveTrue(roomId);
+        if (currentAdmissions.size() >= room.getCapacity()) {
+            throw new IllegalStateException(
+                    "Room has reached maximum capacity"
+            );
+        }
+        admission.setPatient(patient);
+        admission.setRoom(room);
+        admission.setIsActive(true);
+        if (admission.getAdmitDate() == null) {
+            admission.setAdmitDate(LocalDate.now());
+        }
+        return admissionRepository.save(admission);
+    }
 }
