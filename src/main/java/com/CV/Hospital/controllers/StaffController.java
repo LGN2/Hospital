e19074e98bffc21cefb20cb5adbf400ce708642a
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 15:12:13 +0400

    feat(controllers): StaffController added addStaff

--- src/main/java/com/CV/Hospital/controllers/StaffController.java
+++ src/main/java/com/CV/Hospital/controllers/StaffController.java
@@ -1,9 +1,9 @@
 package com.CV.Hospital.controllers;
 
+import com.CV.Hospital.entities.Staff;
 import com.CV.Hospital.services.StaffService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/staff")
@@ -11,4 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class StaffController {
 
     private final StaffService staffService;
+
+    @PostMapping
+    public Staff addStaff(
+            @RequestBody Staff staff,
+            @RequestParam Long departmentId) {
+
+        return staffService.addStaff(staff, departmentId);
+    }
 }
