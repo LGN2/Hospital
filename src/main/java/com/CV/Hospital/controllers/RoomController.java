81d3f3aa199bb7b061c761745395489f62f6985d
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 14:49:03 +0400

    feat(controllers): RoomController added getAllRooms

--- src/main/java/com/CV/Hospital/controllers/RoomController.java
+++ src/main/java/com/CV/Hospital/controllers/RoomController.java
@@ -5,6 +5,8 @@ import com.CV.Hospital.services.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/rooms")
 @RequiredArgsConstructor
@@ -18,5 +20,10 @@ public class RoomController {
             @RequestParam Long hospitalId) {
 
         return roomService.addRoom(room, hospitalId);
+    }
+
+    @GetMapping
+    public List<Room> getAllRooms() {
+        return roomService.getAllRooms();
     }
 }
