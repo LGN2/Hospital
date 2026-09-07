98020d2429cfc9d9c045513d21d1154dafa6c306
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 12:09:41 +0400

    feat(controllers): DepartmentController added addDepartment

--- src/main/java/com/CV/Hospital/controllers/DepartmentController.java
+++ src/main/java/com/CV/Hospital/controllers/DepartmentController.java
@@ -1,9 +1,9 @@
 package com.CV.Hospital.controllers;
 
+import com.CV.Hospital.entities.Department;
 import com.CV.Hospital.services.DepartmentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/departments")
@@ -11,4 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DepartmentController {
 
     private final DepartmentService departmentService;
+
+    @PostMapping
+    public Department addDepartment(
+            @RequestBody Department department,
+            @RequestParam Long hospitalId) {
+
+        return departmentService.addDepartment(department, hospitalId);
+    }
+
 }
