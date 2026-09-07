816626154c0c5e281d5e8714da560e80be64d0d8
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 12:17:43 +0400

    feat(controllers): DoctorController added addDoctor

--- src/main/java/com/CV/Hospital/controllers/DoctorController.java
+++ src/main/java/com/CV/Hospital/controllers/DoctorController.java
@@ -1,9 +1,9 @@
 package com.CV.Hospital.controllers;
 
+import com.CV.Hospital.entities.Doctor;
 import com.CV.Hospital.services.DoctorService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/doctors")
@@ -11,4 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class DoctorController {
 
     private final DoctorService doctorService;
+
+    @PostMapping
+    public Doctor addDoctor(
+            @RequestBody Doctor doctor,
+            @RequestParam Long departmentId,
+            @RequestParam Long hospitalId) {
+        return doctorService.addDoctor(
+                doctor,
+                departmentId,
+                hospitalId
+        );
+    }
 }
