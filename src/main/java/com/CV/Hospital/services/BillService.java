e804be05dc54768068b505da6b61ac222cb14647
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 10:52:11 +0400

    feat(services): BillService added addBill

--- src/main/java/com/CV/Hospital/services/BillService.java
+++ src/main/java/com/CV/Hospital/services/BillService.java
@@ -1,9 +1,15 @@
 package com.CV.Hospital.services;
 
+import com.CV.Hospital.entities.Bill;
+import com.CV.Hospital.entities.Patient;
+import com.CV.Hospital.entities.type.BillStatusType;
 import com.CV.Hospital.repositories.BillRepository;
 import com.CV.Hospital.repositories.PatientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +17,26 @@ public class BillService {
 
     private final BillRepository billRepository;
     private final PatientRepository patientRepository;
+
+    public Bill addBill(Bill bill, Long patientId) {
+        Patient patient =
+                patientRepository.findByIdAndIsActiveTrue(patientId)
+                        .orElseThrow(() ->
+                                new RuntimeException("Patient not found"));
+        if (bill.getAmount() == null ||
+                bill.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException(
+                    "Bill amount must be greater than zero"
+            );
+        }
+        bill.setPatient(patient);
+        bill.setIsActive(true);
+        if (bill.getBillDate() == null) {
+            bill.setBillDate(LocalDate.now());
+        }
+        if (bill.getStatus() == null) {
+            bill.setStatus(BillStatusType.UNPAID);
+        }
+        return billRepository.save(bill);
+    }
 }
