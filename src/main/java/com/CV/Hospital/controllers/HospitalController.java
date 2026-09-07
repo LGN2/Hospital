fd596ad9c0918c504d2cd8f668953222b4aa93c9
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 11:18:08 +0400

    feat(controllers): HospitalController added addHospital

--- src/main/java/com/CV/Hospital/controllers/HospitalController.java
+++ src/main/java/com/CV/Hospital/controllers/HospitalController.java
@@ -1,7 +1,10 @@
 package com.CV.Hospital.controllers;
 
+import com.CV.Hospital.entities.Hospital;
 import com.CV.Hospital.services.HospitalService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HospitalController {
 
     private final HospitalService hospitalService;
+
+    @PostMapping
+    public Hospital addHospital(@RequestBody Hospital hospital) {
+        return hospitalService.addHospital(hospital);
+    }
 }
