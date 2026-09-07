020a382ff8c705e85fa3152f6d3af0ebeb5b3ded
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 14:38:18 +0400

    feat(controllers): AppointmentController added getAppointmentsByPatient

--- src/main/java/com/CV/Hospital/controllers/AppointmentController.java
+++ src/main/java/com/CV/Hospital/controllers/AppointmentController.java
@@ -42,4 +42,11 @@ public class AppointmentController {
 
         return appointmentService.getAppointmentsByDoctor(doctorId);
     }
+
+    @GetMapping("/patient/{patientId}")
+    public List<Appointment> getAppointmentsByPatient(
+            @PathVariable Long patientId) {
+
+        return appointmentService.getAppointmentsByPatient(patientId);
+    }
 }
