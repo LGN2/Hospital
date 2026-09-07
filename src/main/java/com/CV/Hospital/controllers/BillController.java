f1f71ed50e7c97bbbb21a3f7fc5818133edaf685
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 15:02:45 +0400

    feat(controllers): BillController added addBill

--- src/main/java/com/CV/Hospital/controllers/BillController.java
+++ src/main/java/com/CV/Hospital/controllers/BillController.java
@@ -1,9 +1,9 @@
 package com.CV.Hospital.controllers;
 
+import com.CV.Hospital.entities.Bill;
 import com.CV.Hospital.services.BillService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bills")
@@ -11,4 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BillController {
 
     private final BillService billService;
+
+    @PostMapping
+    public Bill addBill(
+            @RequestBody Bill bill,
+            @RequestParam Long patientId) {
+
+        return billService.addBill(bill, patientId);
+    }
 }
