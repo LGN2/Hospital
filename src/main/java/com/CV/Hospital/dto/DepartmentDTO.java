642b35790ead532eee6c071259bd9e13da0cbb14
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-08 08:54:59 +0400

    feat(dto): added DepartmentDTO

---
+++ src/main/java/com/CV/Hospital/dto/DepartmentDTO.java
@@ -0,0 +1,18 @@
+package com.CV.Hospital.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Long hospitalId;
+}
