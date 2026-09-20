1d05cfd1e3ed7afb8d1cf15f1735ad45bf56480a
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-20 21:49:00 +0400

    fix(repositories): updated repo

--- src/main/java/com/CV/Hospital/repositories/PatientRepository.java
+++ src/main/java/com/CV/Hospital/repositories/PatientRepository.java
@@ -2,6 +2,8 @@ package com.CV.Hospital.repositories;
 
 import com.CV.Hospital.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,14 @@ public interface PatientRepository
     List<Patient> findByIsActiveTrue();
     Optional<Patient> findByIdAndIsActiveTrue(Long id);
     List<Patient> findByHospitalIdAndIsActiveTrue(Long hospitalId);
+
+    @Query("""
+        SELECT COUNT(p)
+        FROM Patient p
+        WHERE p.hospital.id = :hospitalId
+        AND p.isActive = true
+        """)
+    Long countActivePatientsByHospital(
+            @Param("hospitalId") Long hospitalId
+    );
 }
