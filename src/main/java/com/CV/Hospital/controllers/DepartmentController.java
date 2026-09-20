f371747fa19f99c445b69406ceb8779dda4791b8
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-20 17:43:27 +0400

    fix(controllers): DepartmentController added DTO

--- src/main/java/com/CV/Hospital/controllers/DepartmentController.java
+++ src/main/java/com/CV/Hospital/controllers/DepartmentController.java
@@ -1,5 +1,6 @@
 package com.CV.Hospital.controllers;
 
+import com.CV.Hospital.dto.DepartmentDTO;
 import com.CV.Hospital.entities.Department;
 import com.CV.Hospital.services.DepartmentService;
 import lombok.RequiredArgsConstructor;
@@ -15,41 +16,50 @@ public class DepartmentController {
     private final DepartmentService departmentService;
 
     @PostMapping
-    public Department addDepartment(
+    public DepartmentDTO addDepartment(
             @RequestBody Department department,
             @RequestParam Long hospitalId) {
 
-        return departmentService.addDepartment(department, hospitalId);
+        return departmentService.convertToDTO(
+                departmentService.addDepartment(department, hospitalId)
+        );
     }
 
     @GetMapping
-    public List<Department> getAllDepartments() {
-        return departmentService.getAllDepartments();
+    public List<DepartmentDTO> getAllDepartments() {
+        return departmentService.convertToDTO(
+                departmentService.getAllDepartments()
+        );
     }
 
     @GetMapping("/{id}")
-    public Department getDepartmentById(@PathVariable Long id) {
-        return departmentService.getDepartmentById(id);
+    public DepartmentDTO getDepartmentById(@PathVariable Long id) {
+        return departmentService.convertToDTO(
+                departmentService.getDepartmentById(id)
+        );
     }
 
     @GetMapping("/hospital/{hospitalId}")
-    public List<Department> getDepartmentsByHospital(
+    public List<DepartmentDTO> getDepartmentsByHospital(
             @PathVariable Long hospitalId) {
 
-        return departmentService.getDepartmentsByHospital(hospitalId);
+        return departmentService.convertToDTO(
+                departmentService.getDepartmentsByHospital(hospitalId)
+        );
     }
 
     @PutMapping("/{id}")
-    public Department updateDepartment(
+    public DepartmentDTO updateDepartment(
             @PathVariable Long id,
             @RequestBody Department department) {
 
-        return departmentService.updateDepartment(id, department);
+        return departmentService.convertToDTO(
+                departmentService.updateDepartment(id, department)
+        );
     }
 
     @DeleteMapping("/{id}")
     public void deleteDepartment(@PathVariable Long id) {
         departmentService.deleteDepartment(id);
     }
-
 }
