b25b4dc294419f8bfe231c4c37b3ca9dacbebd6d
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-20 21:45:51 +0400

    fix(repositories): updated repo

--- src/main/java/com/CV/Hospital/repositories/MedicalRecordRepository.java
+++ src/main/java/com/CV/Hospital/repositories/MedicalRecordRepository.java
@@ -2,6 +2,8 @@ package com.CV.Hospital.repositories;
 
 import com.CV.Hospital.entities.MedicalRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,23 @@ import java.util.Optional;
 @Repository
 public interface MedicalRecordRepository
         extends JpaRepository<MedicalRecord, Long> {
+
     List<MedicalRecord> findByIsActiveTrue();
+
     Optional<MedicalRecord> findByIdAndIsActiveTrue(Long id);
-    List<MedicalRecord> findByPatientIdAndIsActiveTrue(Long patientId);
+
+    List<MedicalRecord> findByPatientIdAndIsActiveTrue(
+            Long patientId
+    );
+
+    @Query("""
+            SELECT m
+            FROM MedicalRecord m
+            WHERE m.patient.id = :patientId
+            AND m.isActive = true
+            ORDER BY m.recordDate DESC
+            """)
+    List<MedicalRecord> findAllActiveMedicalRecordsForPatient(
+            @Param("patientId") Long patientId
+    );
 }
