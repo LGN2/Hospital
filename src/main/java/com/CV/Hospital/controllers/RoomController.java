1f1b19dcc093c6425d7750d4762ebc44e6aafd7d
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-20 18:03:52 +0400

    fix(controllers): updated controller

--- src/main/java/com/CV/Hospital/controllers/RoomController.java
+++ src/main/java/com/CV/Hospital/controllers/RoomController.java
@@ -1,8 +1,8 @@
 package com.CV.Hospital.controllers;
 
 import com.CV.Hospital.dto.RoomDTO;
-import com.CV.Hospital.entities.Room;
 import com.CV.Hospital.services.RoomService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,45 +17,31 @@ public class RoomController {
 
     @PostMapping
     public RoomDTO addRoom(
-            @RequestBody Room room,
-            @RequestParam Long hospitalId) {
-
-        return roomService.convertToDTO(
-                roomService.addRoom(room, hospitalId)
-        );
+            @Valid @RequestBody RoomDTO dto) {
+        return roomService.addRoom(dto);
     }
 
     @GetMapping
     public List<RoomDTO> getAllRooms() {
-        return roomService.convertToDTO(
-                roomService.getAllRooms()
-        );
+        return roomService.getAllRooms();
     }
 
     @GetMapping("/{id}")
     public RoomDTO getRoomById(@PathVariable Long id) {
-        return roomService.convertToDTO(
-                roomService.getRoomById(id)
-        );
+        return roomService.getRoomById(id);
     }
 
     @GetMapping("/hospital/{hospitalId}")
     public List<RoomDTO> getRoomsByHospital(
             @PathVariable Long hospitalId) {
-
-        return roomService.convertToDTO(
-                roomService.getRoomsByHospital(hospitalId)
-        );
+        return roomService.getRoomsByHospital(hospitalId);
     }
 
     @PutMapping("/{id}")
     public RoomDTO updateRoom(
             @PathVariable Long id,
-            @RequestBody Room room) {
-
-        return roomService.convertToDTO(
-                roomService.updateRoom(id, room)
-        );
+            @Valid @RequestBody RoomDTO dto) {
+        return roomService.updateRoom(id, dto);
     }
 
     @DeleteMapping("/{id}")
