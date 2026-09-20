a344963e0c1863ebd24ccb114118ee94c79727e4
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-20 17:51:44 +0400

    fix(dto): added validation

--- src/main/java/com/CV/Hospital/dto/AppointmentDTO.java
+++ src/main/java/com/CV/Hospital/dto/AppointmentDTO.java
@@ -1,10 +1,11 @@
 package com.CV.Hospital.dto;
 
 import com.CV.Hospital.entities.type.AppointmentStatusType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -15,9 +16,21 @@ import java.time.LocalDateTime;
 public class AppointmentDTO {
 
     private Long id;
+
+    @NotNull(message = "Appointment date is required")
+    @Future(message = "Appointment date must be in the future")
     private LocalDateTime appointmentDate;
+
+    @NotBlank(message = "Reason is required")
+    @Size(max = 500)
     private String reason;
+
+    @NotNull(message = "Appointment status is required")
     private AppointmentStatusType status;
+
+    @NotNull(message = "Doctor ID is required")
     private Long doctorId;
+
+    @NotNull(message = "Patient ID is required")
     private Long patientId;
 }
