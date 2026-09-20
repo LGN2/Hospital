a616f5a86325b6249913192f827a32e5da4e0fba
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-20 17:30:38 +0400

    feat(dto): added GuardianDTO

---
+++ src/main/java/com/CV/Hospital/dto/GuardianDTO.java
@@ -0,0 +1,19 @@
+package com.CV.Hospital.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GuardianDTO {
+
+    private Long id;
+    private String name;
+    private String relationship;
+    private String phoneNumber;
+    private Long patientId;
+}
