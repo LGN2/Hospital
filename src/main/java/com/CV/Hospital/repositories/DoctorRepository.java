9cd217403e3c65cbfc16b4774e0fbc62e65f245a
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-20 21:48:36 +0400

    fix(repositories): updated repo

--- src/main/java/com/CV/Hospital/repositories/DoctorRepository.java
+++ src/main/java/com/CV/Hospital/repositories/DoctorRepository.java
@@ -2,6 +2,8 @@ package com.CV.Hospital.repositories;
 
 import com.CV.Hospital.entities.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +16,14 @@ public interface DoctorRepository
     Optional<Doctor> findByIdAndIsActiveTrue(Long id);
     List<Doctor> findByDepartmentIdAndIsActiveTrue(Long departmentId);
     List<Doctor> findByHospitalIdAndIsActiveTrue(Long hospitalId);
+
+    @Query("""
+        SELECT COUNT(d)
+        FROM Doctor d
+        WHERE d.hospital.id = :hospitalId
+        AND d.isActive = true
+        """)
+    Long countActiveDoctorsByHospital(
+            @Param("hospitalId") Long hospitalId
+    );
 }
