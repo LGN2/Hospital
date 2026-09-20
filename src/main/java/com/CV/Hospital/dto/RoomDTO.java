c1b7c38ab96d381706c64d9c78669867f4a64d9b
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-20 17:52:28 +0400

    fix(dto): added validation

--- src/main/java/com/CV/Hospital/dto/RoomDTO.java
+++ src/main/java/com/CV/Hospital/dto/RoomDTO.java
@@ -1,10 +1,8 @@
 package com.CV.Hospital.dto;
 
 import com.CV.Hospital.entities.type.RoomType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 @Data
 @Builder
@@ -13,9 +11,22 @@ import lombok.NoArgsConstructor;
 public class RoomDTO {
 
     private Long id;
+
+    @NotBlank(message = "Room number is required")
+    @Size(max = 20)
     private String roomNumber;
+
+    @NotNull(message = "Floor is required")
+    @PositiveOrZero(message = "Floor cannot be negative")
     private Integer floor;
+
+    @NotNull(message = "Room type is required")
     private RoomType type;
+
+    @NotNull(message = "Capacity is required")
+    @Positive(message = "Capacity must be greater than 0")
     private Integer capacity;
+
+    @NotNull(message = "Hospital ID is required")
     private Long hospitalId;
 }
